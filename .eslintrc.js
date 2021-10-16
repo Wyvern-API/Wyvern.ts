b8609d6e2ccecf93da6ac3c7668f8c0cccdf18f6
Oseryx <oseanisx@gmail.com>
2021-10-17 00:08:03 +0100

    Upgrade workflow with jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
         tsconfigRootDir: __dirname,
         sourceType: 'module'
     },
-    plugins: ['@typescript-eslint/eslint-plugin'],
+    plugins: ['@typescript-eslint/eslint-plugin', 'jest'],
     extends: [
         'plugin:@typescript-eslint/recommended',
         'plugin:prettier/recommended'
